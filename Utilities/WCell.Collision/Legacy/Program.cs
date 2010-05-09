238071c907b39119e3a29b4682c5b9366ba22d5a
XTZGZoReX <xtzgzorex@gmail.com>
2010-05-09 02:36:58 +0200

    Remove unneeded dependency on Microsoft.Xna.Framework.

--- Utilities/WCell.Collision/Legacy/Program.cs
+++ Utilities/WCell.Collision/Legacy/Program.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework;
 using System.Runtime.InteropServices;
+using WCell.Util.Graphics;
 
 namespace WCell.Collision
 {
