795b413895ce05efa4a085566cc08ae098c3cfb9
XTZGZoReX <xtzgzorex@gmail.com>
2010-05-09 03:56:27 +0200

    Update SolutionInfo.cs.

--- Utilities/WCell.SolutionItems/SolutionInfo.cs
+++ Utilities/WCell.SolutionItems/SolutionInfo.cs
@@ -16,11 +16,9 @@
 
 using System.Reflection;
 
-[assembly : AssemblyCompany("WCell Team")]
-[assembly : AssemblyProduct("WCell Server Software Package http://www.wcell.org/")]
-[assembly : AssemblyCopyright("Copyright © 2007 WCell Team")]
-[assembly :
-    AssemblyTrademark(
-        "Licensed under the GNU General Public License Version 2.0 (GPLv2) http://www.gnu.org/licenses/gpl.txt")]
-[assembly : AssemblyVersion("0.5.*")]
-[assembly : AssemblyFileVersion("0.5")]
+[assembly: AssemblyCompany("WCell Team")]
+[assembly: AssemblyProduct("WCell Server Software Package (http://www.wcell.org/)")]
+[assembly: AssemblyCopyright("Copyright © 2010 WCell Team")]
+[assembly: AssemblyTrademark("GNU General Public License Version 2.0 (http://www.gnu.org/licenses/gpl.txt)")]
+[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyFileVersion("0.5")]
