52c366e0ab5f43f2e42de4646e4c382bb4a475ff
XTZGZoReX <xtzgzorex@gmail.com>
2010-07-09 22:23:03 +0200

    * Optimization in PacketCrypt (just commented some example/explanative (is that a word?) code out).

--- Core/WCell.Core/Cryptography/PacketCrypt.cs
+++ Core/WCell.Core/Cryptography/PacketCrypt.cs
@@ -10,13 +10,20 @@ namespace WCell.Core.Cryptography
 	public class PacketCrypt
 	{
 		/// <summary>
+		/// The amount of bytes to drop from the stream initially.
+		/// 
+		/// This is to resist the FMS attack.
+		/// </summary>
+		public const int DropN = 1024;
+
+		/// <summary>
 		/// This is the key the client uses to encrypt its packets
 		/// This is also the key the server uses to decrypt the packets
 		/// </summary>
 		private static readonly byte[] ServerDecryptionKey =
 			{
 				0xC2, 0xB3, 0x72, 0x3C, 0xC6, 0xAE, 0xD9, 0xB5,
-                0x34, 0x3C, 0x53, 0xEE, 0x2F, 0x43, 0x67, 0xCE
+				0x34, 0x3C, 0x53, 0xEE, 0x2F, 0x43, 0x67, 0xCE
 			};
 
 		/// <summary>
@@ -26,7 +33,7 @@ namespace WCell.Core.Cryptography
 		private static readonly byte[] ServerEncryptionKey =
 			{
 				0xCC, 0x98, 0xAE, 0x04, 0xE8, 0x97, 0xEA, 0xCA,
-                0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57
+				0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57
 			};
 
 		// This is valid as HMAC-SHA1 transforms can be reused
@@ -48,22 +55,22 @@ namespace WCell.Core.Cryptography
 			var decryptHash = s_decryptClientDataHMAC.ComputeHash(sessionKey);
 
 			// Used by the client to decrypt packets sent by the server
-			var decryptServerData = new ARC4(encryptHash); // CLIENT-SIDE
+			//var decryptServerData = new ARC4(encryptHash); // CLIENT-SIDE
 			// Used by the server to decrypt packets sent by the client
 			decryptClientData = new ARC4(decryptHash); // SERVER-SIDE
 			// Used by the server to encrypt packets sent to the client
 			encryptServerData = new ARC4(encryptHash); // SERVER-SIDE
 			// Used by the client to encrypt packets sent to the server
-			var encryptClientData = new ARC4(decryptHash); // CLIENT-SIDE
+			//var encryptClientData = new ARC4(decryptHash); // CLIENT-SIDE
 
 			// Use the 2 encryption objects to generate a common starting point
-			var syncBuffer = new byte[1024];
+			var syncBuffer = new byte[DropN];
 			encryptServerData.Process(syncBuffer, 0, syncBuffer.Length);
-			encryptClientData.Process(syncBuffer, 0, syncBuffer.Length);
+			//encryptClientData.Process(syncBuffer, 0, syncBuffer.Length);
 
 			// Use the 2 decryption objects to generate a common starting point
-			syncBuffer = new byte[1024];
-			decryptServerData.Process(syncBuffer, 0, syncBuffer.Length);
+			syncBuffer = new byte[DropN];
+			//decryptServerData.Process(syncBuffer, 0, syncBuffer.Length);
 			decryptClientData.Process(syncBuffer, 0, syncBuffer.Length);
 		}
 
