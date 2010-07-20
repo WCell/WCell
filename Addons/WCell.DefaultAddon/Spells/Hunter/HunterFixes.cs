0cc77705cd1647f7d4009804e1a9ecbd74862449
Shyax <polo455@hotmail.fr>
2010-07-20 21:15:39 +0200

    - Fix Black Arrow with the RAP bonus.

--- Addons/WCell.DefaultAddon/Spells/Hunter/HunterFixes.cs
+++ Addons/WCell.DefaultAddon/Spells/Hunter/HunterFixes.cs
@@ -74,9 +74,15 @@ namespace WCell.Addons.Default.Spells.Hunter
         {
             // Moongonse Bite does incorrect damage ($AP*0.2 + $m1)
             SpellLineId.HunterMongooseBite.Apply(spell =>
-            {
-                spell.Effects[0].APValueFactor = 0.2f;
-            });
+                {
+                    spell.Effects[0].APValueFactor = 0.2f;
+                });
+
+            // Black Arrow does incorrect damage ($RAP*0.1 + $m1)
+            SpellLineId.HunterSurvivalBlackArrow.Apply(spell =>
+                {
+                    spell.Effects[0].APValueFactor = 0.1f;
+                });
         }
         #endregion
     }
