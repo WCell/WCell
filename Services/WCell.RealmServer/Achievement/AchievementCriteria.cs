620e961a869f26c984a1cf627f4f93fcc8c845c2
Domiii <dominikseifert@yahoo.de>
2010-07-29 08:10:32 +0800

    - missing files like always (git should warn you about that)

---
+++ Services/WCell.RealmServer/Achievement/AchievementCriteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using WCell.Constants.Achievements;
+using WCell.Constants.NPCs;
+
+namespace WCell.RealmServer.Achievement
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public abstract class AchievementCriteriaEntry
+	{
+		public AchievementCriteria Criteria;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class KillCreatureAchievementCriteriaEntry : AchievementCriteriaEntry
+	{
+		public NPCId CreatureId;
+		public int CreatureCount;
+	}
+}
