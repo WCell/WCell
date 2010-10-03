bb97a409f36dbdd2e279c06f1a3c764707ff3284
Shadowstep0705 <alexander.cogneau@hotmail.be>
2010-10-03 19:42:53 +0200

    Added a default file where users can enter their GameObject fixes the file is DefaultAddon/GameObjects/GameObjectFixes.cs
    Signed-off-by: Shadowstep0705 <alexander.cogneau@hotmail.be>

---
+++ Addons/WCell.DefaultAddon/GameObjects/GameObjectFixes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCell.Constants.GameObjects;
+using WCell.Core.Initialization;
+using WCell.RealmServer.Misc;
+using WCell.RealmServer.GameObjects;
+using WCell.RealmServer.GameObjects.GOEntries;
+using WCell.RealmServer.GameObjects.Handlers;
+
+namespace WCell.Addons.Default.GameObjects
+{
+    public static class GameObjectFixes
+    {
+        [Initialization]
+        [DependentInitialization(typeof(GOMgr))]
+        public static void InitGOs()
+        {
+           //Add your GO fixes here
+           //View the GO API for more info
+        }
+
+    }
+}
