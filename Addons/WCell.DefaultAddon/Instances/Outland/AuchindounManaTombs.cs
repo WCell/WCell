28236e1701d3b67b89f42603b72073c6ac6e7e6f
Shyax <polo455@hotmail.fr>
2010-10-10 16:36:40 +0200

    - Implement Pandemonius in Mana Tombs (To test)

--- Addons/WCell.DefaultAddon/Instances/Outland/AuchindounManaTombs.cs
+++ Addons/WCell.DefaultAddon/Instances/Outland/AuchindounManaTombs.cs
@@ -1,5 +1,12 @@
+using System;
+using WCell.Constants.NPCs;
+using WCell.Constants.Spells;
+using WCell.Core.Initialization;
+using WCell.RealmServer.AI.Brains;
+using WCell.RealmServer.Entities;
 using WCell.RealmServer.Instances;
-
+using WCell.RealmServer.NPCs;
+using WCell.RealmServer.Spells;
 ///
 /// This file was automatically created, using WCell's CodeFileWriter
 /// Date: 9/1/2009
@@ -7,8 +14,81 @@ using WCell.RealmServer.Instances;
 
 namespace WCell.Addons.Default.Instances
 {
-	public class AuchindounManaTombs : DungeonInstance
-	{
-	}
+    public class AuchindounManaTombs : DungeonInstance
+    {
+        #region Setup Content
+        private static NPCEntry pandemoniusEntry;
+
+        [Initialization]
+        [DependentInitialization(typeof(NPCMgr))]
+        public static void InitNPCs()
+        {
+            //Pandemonius
+            pandemoniusEntry = NPCMgr.GetEntry(NPCId.Pandemonius);
+            pandemoniusEntry.BrainCreator = pandemonius => new PandemoniusBrain(pandemonius);
+            SpellId[] pandemoniusSpells = new SpellId[2] { SpellId.VoidBlast, SpellId.DarkShell };
+            pandemoniusEntry.AddSpells(pandemoniusSpells);
+            Random spellCooldownVoidBlast = new Random();
+            SpellHandler.Apply(spell => spell.CooldownTime = spellCooldownVoidBlast.Next(8000, 23000), pandemoniusSpells[0]);
+            SpellHandler.Apply(spell => spell.CooldownTime = 20000, pandemoniusSpells[1]);
+        }
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Pandemonius
+        public class PandemoniusBrain : MobBrain
+        {
+            private Random random = new Random();
+            private uint voidBlastCounter = 0;
+
+            public PandemoniusBrain(NPC pandemonius)
+                : base(pandemonius) { }
+
+            public override void OnEnterCombat()
+            {
+                switch (random.Next(3))
+                {
+                    case 0: m_owner.PlayTextAndSoundById(-1557008); break;
+                    case 1: m_owner.PlayTextAndSoundById(-1557009); break;
+                    case 2: m_owner.PlayTextAndSoundById(-1557010); break;
+                }
+                base.OnEnterCombat();
+            }
+
+            public override void OnDeath()
+            {
+                m_owner.PlayTextAndSoundById(-1557013);
+                base.OnDeath();
+            }
+
+            public override void OnKilled(Unit killerUnit, Unit victimUnit)
+            {
+                switch (random.Next(2))
+                {
+                    case 0: m_owner.PlayTextAndSoundById(-1557011); break;
+                    case 1: m_owner.PlayTextAndSoundById(-1557012); break;
+                }
+                base.OnKilled(killerUnit, victimUnit);
+            }
+            public override void OnDamageDealt(RealmServer.Misc.IDamageAction action)
+            {
+                if (action.Spell == SpellHandler.Get(SpellId.VoidBlast))
+                {
+                    m_owner.Say(m_owner.Target.Name + " shifts into the void...");
+                    ++voidBlastCounter;
+                    if (voidBlastCounter == 5)
+                    {
+                        SpellHandler.Apply(spell => spell.CooldownTime = (random.Next(15000, 25000)), SpellId.VoidBlast);
+                        voidBlastCounter = 0;
+                    }
+                    base.OnDamageDealt(action);
+                }
+            }
+        };
+        #endregion
+    }
 
 }
