efb1f70f78e02e68bfe7fd032b4cea5f4b7b9311
Domiii <dominikseifert@yahoo.de>
2011-01-10 13:18:16 +0800

    - fixed Addon project settings for Release mode; release mode should work now

--- Addons/WCell.DefaultAddon/Lang/DefaultAddonLocalizer.cs
+++ Addons/WCell.DefaultAddon/Lang/DefaultAddonLocalizer.cs
@@ -21,6 +21,8 @@ namespace WCell.Addons.Default.Lang
 		[Initialization(InitializationPass.First)]
 		public static void InitializeLocalizer()
 		{
+			var k = AddonMsgKey.WSOnStart;
+			Console.WriteLine("Hello + " + k);
 			try
 			{
 				instance = new DefaultAddonLocalizer(ClientLocale.English,RealmServerConfiguration.DefaultLocale, DefaultAddon.Instance.LangDir);
