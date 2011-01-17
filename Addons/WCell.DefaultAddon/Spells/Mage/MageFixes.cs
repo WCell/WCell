cca9a8afcb5f612108a986f782308a575ddaf8fa
Dagor <roli@.(none)>
2011-01-17 22:04:16 +0800

    [01] Missing Spell in Mage Armor AuraGroup

--- Addons/WCell.DefaultAddon/Spells/Mage/MageFixes.cs
+++ Addons/WCell.DefaultAddon/Spells/Mage/MageFixes.cs
@@ -72,7 +72,7 @@ namespace WCell.Addons.Default.Spells.Mage
 			}, SpellId.ClassSkillLivingBombRank3);
 
 			// These spells cancel eachother
-			AuraHandler.AddAuraGroup(SpellLineId.MageFrostArmor, SpellLineId.MageIceArmor, SpellLineId.MageMageArmor);
+			AuraHandler.AddAuraGroup(SpellLineId.MageFrostArmor, SpellLineId.MageIceArmor, SpellLineId.MageMageArmor,SpellLineId.MageMoltenArmor);
 
 			// Mana gems don't have a limit
 			SpellHandler.Apply(spell =>
