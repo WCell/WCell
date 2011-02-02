53467fee981d293102e0e22024ba4e4aa5c06ee2
bKastner <bk.horizon@gmail.com>
2011-02-01 21:46:51 -0600

    Revert "Arcane Potency should increase your crit chance by 7% or 15%."
    This reverts commit f21963d810934d50debaae4eba3c7566a995d72e.

--- Addons/WCell.DefaultAddon/Spells/Mage/MageArcaneFixes.cs
+++ Addons/WCell.DefaultAddon/Spells/Mage/MageArcaneFixes.cs
@@ -5,8 +5,6 @@ using System.Text;
 using WCell.Constants.Spells;
 using WCell.Core.Initialization;
 using WCell.RealmServer.Spells;
-using WCell.RealmServer.Misc;
-using WCell.RealmServer.Spells.Auras.Misc;
 
 namespace WCell.Addons.Default.Spells.Mage
 {
@@ -18,21 +16,6 @@ namespace WCell.Addons.Default.Spells.Mage
 			// conjure water and food don't have any per level bonus
 			SpellLineId.MageConjureFood.Apply(spell => spell.ForeachEffect(effect => effect.RealPointsPerLevel = 0));
 			SpellLineId.MageConjureWater.Apply(spell => spell.ForeachEffect(effect => effect.RealPointsPerLevel = 0));
-
-            SpellLineId.MageArcaneArcanePotency.Apply(spell =>
-            {
-                var effect = spell.GetEffect(AuraType.Dummy);
-                effect.AuraEffectHandlerCreator = () => new ArcanePotencyHandler();
-            });
 		}
 	}
-
-    public class ArcanePotencyHandler : AttackEventEffectHandler
-    {
-        public override void OnAttack(DamageAction action)
-        {
-            if (!action.Spell.IsTriggeredSpell && !action.IsWeaponAttack)
-                action.AddBonusCritChance(EffectValue);
-        }
-    }
 }
