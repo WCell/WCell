522da879949e0fa093f47b7202543791fac59916
bKastner <bk.horizon@gmail.com>
2011-02-02 13:26:52 -0600

    Merged in bKastner's Changes:
    Prevent ModArmorByPercentOfIntellectHandler from removing less intellect then was originally added.

--- Services/WCell.RealmServer/Spells/Auras/Mod/ModArmorByPercentOfIntellectHandler.cs
+++ Services/WCell.RealmServer/Spells/Auras/Mod/ModArmorByPercentOfIntellectHandler.cs
@@ -7,14 +7,16 @@ namespace WCell.RealmServer.Spells.Auras.Mod
 {
     class ModArmorByPercentOfIntellectHandler : AuraEffectHandler
     {
+        private int value;
         protected override void Apply()
         {
-            Owner.Armor += (Owner.Intellect * 100 + 50) / EffectValue;
+            value = (Owner.Intellect * 100 + 50) / EffectValue;
+            Owner.AddResistanceBuff(Constants.DamageSchool.Physical, value);
         }
 
         protected override void Remove(bool cancelled)
         {
-            Owner.Armor -= (Owner.Intellect * 100 + 50) / EffectValue;
+            Owner.RemoveResistanceBuff(Constants.DamageSchool.Physical, value);
         }
     }
 }
