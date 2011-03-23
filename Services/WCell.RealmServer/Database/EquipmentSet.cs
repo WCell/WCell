2b60c21d4294bb089b678631429b6237191cb921
Pepsi1x1 <davyt23@hotmail.co.uk>
2011-03-23 15:18:59 +0000

    *Missing line

--- Services/WCell.RealmServer/Database/EquipmentSet.cs
+++ Services/WCell.RealmServer/Database/EquipmentSet.cs
@@ -23,7 +23,7 @@ namespace WCell.RealmServer.Database
 		public static readonly IList<EquipmentSet> EmptyList = new List<EquipmentSet>(1);
 
 		private static readonly NHIdGenerator m_idGenerator =
-			new NHIdGenerator(typeof(EquipmentSet), "EquipmentSets", "EntityLowId");
+			new NHIdGenerator(typeof(EquipmentSet), "equipmentsets", "EntityLowId");
 
 		[PrimaryKey(PrimaryKeyType.Assigned, "EntityLowId")]
 		private long lowId
