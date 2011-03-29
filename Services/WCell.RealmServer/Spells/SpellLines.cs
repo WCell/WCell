3c3879678ecf4d47121c9db5240af23cd7a2f40e
Pepsi1x1 <davyt23@hotmail.co.uk>
2011-03-29 20:06:41 +0100

    *More out of range fixes

--- Services/WCell.RealmServer/Spells/SpellLines.cs
+++ Services/WCell.RealmServer/Spells/SpellLines.cs
@@ -5,6 +5,7 @@ using System.Text;
 using WCell.Constants;
 using WCell.Core.Initialization;
 using WCell.Constants.Spells;
+using WCell.Util.Logging;
 
 namespace WCell.RealmServer.Spells
 {
@@ -12,6 +13,7 @@ namespace WCell.RealmServer.Spells
 	{
 		public static readonly SpellLine[][] SpellLinesByClass = new SpellLine[(int)ClassId.End][];
 		public static readonly SpellLine[] ById = new SpellLine[(int)SpellLineId.End + 100];
+	    public static Logger log = LogManager.GetCurrentClassLogger();
 
 		public static SpellLine GetLine(this SpellLineId id)
 		{
@@ -20,6 +22,12 @@ namespace WCell.RealmServer.Spells
 
 		public static SpellLine[] GetLines(ClassId clss)
 		{
+            if((int)clss >= SpellLinesByClass.Length)
+            {
+                log.Error("Class out of range of SpellLine array {1}:{0} SpellLinesByClass.Length {2}", clss, (int)clss,
+                              SpellLinesByClass.Length);
+                return null;
+            }
 			return SpellLinesByClass[(int)clss];
 		}
 
