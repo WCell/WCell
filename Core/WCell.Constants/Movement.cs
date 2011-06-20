87909f4312c5e55daf3aef9a7d85684b1fbd3ca2
Pepsi1x1 <davyt23@hotmail.co.uk>
2011-06-20 10:33:36 +0100

    *Update a bunch of enums thanks to Phazerz!

--- Core/WCell.Constants/Movement.cs
+++ Core/WCell.Constants/Movement.cs
@@ -26,124 +26,122 @@ namespace WCell.Constants
 	}
 
 
-	[Flags]
-	public enum MovementFlags : uint
-	{
-		None = 0,
-		Forward = 0x1,
-		Backward = 0x2,
-		StrafeLeft = 0x4,
-		StrafeRight = 0x8,
-		Left = 0x10,
-		Right = 0x20,
-		PitchUp = 0x40,
-		PitchDown = 0x80,
-		Walk = 0x100,
-		OnTransport = 0x200,
-		Levitating = 0x400,
+    [Flags]
+    public enum MovementFlags : uint
+    {
+        None = 0x0,
+        Forward = 0x1,
+        Backward = 0x2,
+        StrafeLeft = 0x4,
+        StrafeRight = 0x8,
+        TurnLeft = 0x10,
+        TurnRight = 0x20,
+        PitchUp = 0x40,
+        PitchDown = 0x80,
+        WalkMode = 0x100,
+        OnTransport = 0x200,
+        DisableGravity = 0x400,
+        Root = 0x800,
+        Falling = 0x1000,
+        FallingFar = 0x2000,
+        PendingStop = 0x4000,
+        PendingStrafeStop = 0x8000,
+        PendingForward = 0x10000,
+        PendingBackward = 0x20000,
+        PendingStrafeLeft = 0x40000,
+        PendingStrafeRight = 0x80000,
+        PendingRoot = 0x100000,
+        Swimming = 0x200000,
+        Ascending = 0x400000,
+        Descending = 0x800000,
+        CanFly = 0x1000000,
+        Flying = 0x2000000,
+        /// <summary>
+        /// Used For Flight Paths, also called WalkableRedirection?
+        /// </summary>
+        SplineElevation = 0x4000000,
+        SplineEnabled = 0x8000000,
+        Waterwalking = 0x10000000,
+        CanSafeFall = 0x20000000,
+        Hover = 0x40000000,
+        /// <summary>
+        /// Client has changes that haven't been sent back to server. Server should never see this flag
+        /// </summary>
+        LocalDirty = 0x80000000,
 
-		/// <summary>
-		/// Used in Movement block
-		/// </summary>
-		UnitFlying = 0x800,//FlyUnk1 = 0x00000800,
+        MaskDirections = Forward | Backward | StrafeLeft | StrafeRight | TurnLeft | TurnRight | PitchUp | PitchDown,
+        MaskMoving = Forward | Backward | StrafeLeft | StrafeRight | TurnLeft | TurnRight | Falling | FallingFar | Ascending | Descending,
 
-		/// <summary>
-		/// 0x1000
-		/// </summary>
-		Falling = 0x1000, // 2.4.3 confirmed. Lua_IsFalling
 
-		Flag_13_0x2000 = 0x2000,
-		Flag_14_0x4000 = 0x4000,
-		Flag_15_0x8000 = 0x8000,
-		Flag_16_0x10000 = 0x10000,
-		Flag_17_0x20000 = 0x20000,
-		Flag_18_0x40000 = 0x40000,
-		Flag_19_0x80000 = 0x80000,
-		Flag_20_0x100000 = 0x100000,
+        // GameObject Specific Flags - Never seen or influenced directly by the server, client-side only.
+        AwaitingLoad = 0x08000000,
+    }
 
-		/// <summary>
-		/// 0x200000
-		/// </summary>
-		Swimming = 0x200000, // 2.4.3 confirmed. Lua_IsSwimming
+    [Flags]
+    public enum MovementFlags2 : ushort
+    {
+        None = 0x0000,
+        PreventStrafe = 0x0001,	// TODO: Recheck preventing AscendDescend. Prevent strafe confirmed
+        PreventJumping = 0x0002,
+        DisableCollision = 0x0004,
+        FullSpeedTurning = 0x0008,
+        FullSpeedPitching = 0x0010,
+        AlwaysAllowPitching = 0x0020,
+        IsVehicleExitVoluntary = 0x0040,
+        IsJumpSplineInAir = 0x0080,
+        IsAnimTierInTrans = 0x0100,
+        PreventChangePitch = 0x0200,
+        InterpolateMove = 0x0400,	// Interpolation is player only
+        InterpolateTurning = 0x0800,
+        InterpolatePitching = 0x1000,
+        VehiclePassengerIsTransitionAllowed = 0x2000,
+        CanTransitionBetweenSwimAndFly = 0x4000,
+        Flag_0x8000 = 0x8000,	// Used in CGUnit_C::UpdateFlightStatus, prevent flight and swimming? or prevent falling?
 
-		Ascend = 0x400000,
-		Descend = 0x800000,
-		Flag_24_0x1000000 = 0x1000000,
+        // The client also reuses some flags to indicate other states when
+        // sending movement packets back to the server 
+        Status_400 = 0x400,	//	This flag means that the "TransportTime2" field of the CMovementStatus should be used
+        Status_800 = 0x800,	//	This flag means the client is currently falling
 
-		/// <summary>
-		/// 0x2000000
-		/// </summary>
-		Flying = 0x2000000, // 2.4.3 confirmed for players. Lua_IsFlying
 
-		/// <summary>
-		/// 0x4000000
-		/// </summary>
-		Spline = 0x4000000, // ?
-		SplinePath = 0x8000000,
+        InterpMask = InterpolateMove | InterpolateTurning | InterpolatePitching
+    }
 
-		Waterwalking = 0x10000000,
-		SafeFall = 0x20000000,
-		Leviate = 0x40000000,
-		Flag_31_0x80000000 = 0x80000000,
+    [Flags]
+    public enum SplineFlags : uint
+    {
+        None = 0x00000000,
 
-		//IsTranslating = 0xC0100F,
-		IsTranslating = Forward | Backward | StrafeLeft | StrafeRight | Falling | Ascend | Descend,
+        // 0x00 - 0x7 is the AnimationTier
 
-		Orientation = PitchDown | PitchUp | Left | Right,
-
-		All = uint.MaxValue
-	}
-
-	[Flags]
-	public enum MovementFlags2 : uint
-	{
-		None = 0,
-		/// <summary>        
-		/// 0x20        
-		/// </summary>        
-		AlwaysAllowPitching = 0x20,
-		InterpMask = MoveFlag2_10_0x400 | MoveFlag2_11_0x800 | MoveFlag2_12_0x1000,
-
-		MoveFlag2_0_0x1 = 0x1,//0
-		MoveFlag2_1_0x2 = 0x2,//1
-		MoveFlag2_2_0x4 = 0x4,//2
-		MoveFlag2_3_0x8 = 0x8,//3
-		MoveFlag2_4_0x10 = 0x10,//4
-		MoveFlag2_6_0x40 = 0x40,//6
-		MoveFlag2_7_0x80 = 0x80,//7
-		MoveFlag2_8_0x100 = 0x100,//8
-		MoveFlag2_9_0x200 = 0x200,//9
-		MoveFlag2_10_0x400 = 0x400,//10
-		MoveFlag2_11_0x800 = 0x800,//11
-		MoveFlag2_12_0x1000 = 0x1000,//12
-		MoveFlag2_13_0x2000 = 0x2000,//13
-		MoveFlag2_14_0x4000 = 0x4000,//14
-		MoveFlag2_15_0x8000 = 0x8000,//15
-		MoveFlag2_16_0x10000 = 0x10000,//16
-		MoveFlag2_17_0x20000 = 0x20000,//17
-		MoveFlag2_18_0x40000 = 0x40000,//18
-		MoveFlag2_19_0x80000 = 0x80000,//19
-		MoveFlag2_20_0x100000 = 0x100000,//20
-		MoveFlag2_21_0x200000 = 0x200000,//21
-		MoveFlag2_22_0x400000 = 0x400000,//22
-		MoveFlag2_23_0x800000 = 0x800000,//23
-		MoveFlag2_24_0x1000000 = 0x1000000,//24
-		MoveFlag2_25_0x2000000 = 0x2000000,//25
-		MoveFlag2_26_0x4000000 = 0x4000000,//26
-		MoveFlag2_27_0x8000000 = 0x8000000,//27
-		MoveFlag2_28_0x10000000 = 0x10000000,//28
-		MoveFlag2_29_0x20000000 = 0x20000000,//29
-		MoveFlag2_30_0x40000000 = 0x40000000,//30
-		MoveFlag2_31_0x80000000 = 0x80000000,//31
-	}
-
-	[Flags]
-	public enum SplineFlags : uint
-	{
-		None = 0,
-
-		XYZ = 0x8000,
-		GUID = 0x10000,
-		Orientation = 0x20000
-	}
+        Flag_0x8 = 0x00000008,
+        SplineSafeFall = 0x00000010,
+        Flag_0x20 = 0x00000020,
+        Flag_0x40 = 0x00000040,
+        Flag_0x80 = 0x00000080,
+        Done = 0x00000100,
+        Falling = 0x00000200,	// Affects elevation computation, can't be combined with Parabolic flag
+        NotSplineMover = 0x00000400,
+        Parabolic = 0x00000800,	// Affects elevation computation, can't be combined with Falling flag. Alternate name is Jumping
+        Walkmode = 0x00001000,
+        Flying = 0x00002000,
+        Knockback = 0x00004000,	// Model orientation fixed
+        FinalFacePoint = 0x00008000,
+        FinalFaceTarget = 0x00010000,
+        FinalFaceAngle = 0x00020000,
+        CatmullRom = 0x00040000,
+        Cyclic = 0x00080000,	// Movement by cycled spline
+        EnterCycle = 0x00100000,	// Everytimes appears with cyclic flag in monster move packet, erases first spline vertex after first cycle done
+        AnimationTier = 0x00200000,	// Plays animation after some time passed
+        Frozen = 0x00400000,	// Will never arrive
+        Unknown5 = 0x00800000,	// Transport?
+        Unknown6 = 0x01000000,	// ExitVehicle?
+        Unknown7 = 0x02000000,
+        Unknown8 = 0x04000000,
+        Backward = 0x08000000,
+        UsePathSmoothing = 0x10000000,	// SplineType 2
+        SplineCanSwim = 0x20000000,
+        UncompressedPath = 0x40000000,	// Read pathpoints uncompressed in monster move
+        Unknown13 = 0x80000000
+    }
 }
