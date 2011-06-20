da626c224b0fd4d3e54169c36ee8fc8a95535d96
Dagor <roli@.(none)>
2011-06-20 16:54:14 +0200

    - more work on mirror image

---
+++ Services/WCell.RealmServer/Spells/Effects/ScriptEffect.cs
@@ -0,0 +1,17 @@
+﻿using WCell.RealmServer.Entities;
+
+namespace WCell.RealmServer.Spells.Effects
+{
+	public class ScriptEffectHandler : SpellEffectHandler
+	{
+		public ScriptEffectHandler(SpellCast cast, SpellEffect effect)
+			: base(cast, effect)
+		{
+		}
+
+		protected override void Apply(WorldObject target)
+		{
+			
+		}
+	}
+}
