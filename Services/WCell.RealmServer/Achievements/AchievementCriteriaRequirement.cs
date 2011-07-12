7dcc62464af13cac3c006b015c4ca0e8333280dc
lYric0 <bk.horizon@gmail.com>
2011-07-11 19:37:59 -0500

    Fix RequirementLevel achievements below level 55 rewarding to Death Knights.

--- Services/WCell.RealmServer/Achievements/AchievementCriteriaRequirement.cs
+++ Services/WCell.RealmServer/Achievements/AchievementCriteriaRequirement.cs
@@ -136,6 +136,13 @@ namespace WCell.RealmServer.Achievements
         {
             if (target == null)
                 return false;
+            var charTarget = target as Character;
+            if (charTarget != null)
+            {
+                if (charTarget.Class == ClassId.DeathKnight)
+                    if (Value1 < 55)
+                        return false; // Do not reward achievement for death knights when level < 55
+            }
             return target.Level >= Value1;
         }
     }
