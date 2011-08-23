9c1c6ab45cdef9dc731b0e13d86e7a6389196f63
fubeca <salaciouscrumb@netzero.net>
2011-08-22 21:19:17 -0600

    Fixed string pulling on paths which overlap in 2D.
    Signed-off-by: fubeca <salaciouscrumb@netzero.net>

---
+++ WCell-Terrain/WCell.Terrain/Pathfinding/NodeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCell.Util.Graphics;
+
+namespace WCell.Terrain.Pathfinding
+{
+    public struct NodeItem
+    {
+        public Vector3 Node;
+        public SearchItem Item;
+
+        public NodeItem(Vector3 node, SearchItem item)
+        {
+            Node = node;
+            Item = item;
+        }
+    }
+}
---
+++ WCell-Terrain/WCell.Terrain/Pathfinding/PathExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCell.Core.Paths;
+using WCell.Util.Graphics;
+
+namespace WCell.Terrain.Pathfinding
+{
+    public static class PathExtensions
+    {
+        public static void Add(this Path path, List<NodeItem> pairs)
+        {
+            var pathNodes = new List<Vector3>(pairs.Count);
+            foreach (var pair in pairs)
+            {
+                pathNodes.Add(pair.Node);
+            }
+            path.Add(pathNodes);
+        }
+    }
+}
