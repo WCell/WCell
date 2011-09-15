030963fb99ab71acea70c257a50d45c1319a2387
Pepsi1x1 <davyt23@hotmail.co.uk>
2011-09-15 12:30:23 +0100

    *Bunch of LFG/LFD stuff, enums, packet structures etc

--- Services/WCell.RealmServer/LFG/LFGMgr.cs
+++ Services/WCell.RealmServer/LFG/LFGMgr.cs
@@ -1,7 +1,14 @@
-﻿namespace WCell.RealmServer.LFG
+﻿using System.Collections.Generic;
+using WCell.Constants.Spells;
+using WCell.Core;
+using WCell.RealmServer.Entities;
+
+namespace WCell.RealmServer.LFG
 {
 	public static class LFGMgr
 	{
-		
+		private static SpellId DungeonDeserter = SpellId.DungeonDeserter;
+		private static SpellId DungeonCooldown = SpellId.DungeonCooldown;
+		private static SpellId LuckOfTheDraw = SpellId.LuckOfTheDraw;
 	}
 }
