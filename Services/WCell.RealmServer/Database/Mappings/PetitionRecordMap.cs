9131d01050d14f2f71cd87ca4a3a27202ce74527
Shyax <polo455@hotmail.fr>
2013-12-27 13:59:57 +0100

    First attempt to convert PetitionRecord to fluent Add a function in DatabaseProvider to make finding the first item easier

---
+++ Services/WCell.RealmServer/Database/Mappings/PetitionRecordMap.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using WCell.RealmServer.Items;
+
+namespace WCell.RealmServer.Database.Mappings
+{
+    internal class PetitionRecordMap : ClassMap<PetitionRecord>
+    {
+        public PetitionRecordMap()
+        {
+            Id(x => x.OwnerId);
+            Map(x => x.Type).Not.Nullable();
+            Map(x => x.ItemId).Not.Nullable();
+            Map(x => x.Name).Not.Nullable().Unique();
+            HasMany(x => x.SignedIds).Element("SignedIds");
+        }
+    }
+}
